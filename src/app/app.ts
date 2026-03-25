1f04966f21bd1fa0f07b2d01f7a4388e0c2ba384
Saulo McChelso <admin@azjob.com.br>
2026-03-24 23:17:21 -0300

    feat: Add Vercel Analytics dependency and integrate it into the application.

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { ThemeService } from './shared/services/theme.service';
 import { LanguageService } from './shared/services/language.service';
 import { injectSpeedInsights } from '@vercel/speed-insights';
+import { inject as injectAnalytics } from '@vercel/analytics';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +16,7 @@ export class App {
   private readonly languageService = inject(LanguageService);
 
   constructor() {
+    injectAnalytics();
     injectSpeedInsights();
   }
 }
